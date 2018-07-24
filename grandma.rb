5ae7c48da3dfe3563c847b7ddd97dd5ffd3c6315
Sharmaine Requerme <srqrme@gmail.com>
2018-07-24 20:37:29 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,5 +15,7 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif string == string.upcase && != "I love you grandma!"
+    return "NO, NOT SINCE 1938!"
   end
 end
